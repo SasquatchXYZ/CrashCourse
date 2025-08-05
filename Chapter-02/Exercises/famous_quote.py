37420e8438848d6a652eb2fa5b8dfadef19e0b19
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-08-04 23:44:02 -0400

    Completed chapter 2

---
+++ Chapter-02/Exercises/famous_quote.py
@@ -0,0 +1,3 @@
+author_name = 'Albert Einstein'
+quote = '"A person who never made a mistake never tried anything new."'
+print(f"{author_name} once said, {quote}")
---
+++ Chapter-02/Exercises/famous_quote_two.py
@@ -0,0 +1,4 @@
+author_name = 'Albert Einstein'
+quote = '"A person who never made a mistake never tried anything new."'
+message = f"{author_name} once said, {quote}"
+print(message)
