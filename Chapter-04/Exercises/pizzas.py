dd5a19236ac938df34e37d40a8e9c00364829d9b
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-08-06 23:34:52 -0400

    Completed chapter 4

---
+++ Chapter-04/Exercises/pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ['pepperoni', 'margherita', 'meat-lovers']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
---
+++ Chapter-04/foods.py
@@ -0,0 +1,18 @@
+my_foods = ['steak', 'eggs', 'ground beef']
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+my_foods.append('kimchi')
+friend_foods.append('pizza')
+
+print("\n------------------------------------\n")
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
