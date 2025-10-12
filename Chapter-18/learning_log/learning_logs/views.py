4ee04587dc8563051b8834cdf473ffd0e5b6ddb2
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-10-11 23:00:33 -0400

    Add page for adding a new topic

--- Chapter-18/learning_log/learning_logs/views.py
+++ Chapter-18/learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -19,5 +20,22 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     selected_topic = Topic.objects.get(id=topic_id)
     entries = selected_topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
+    context = {'topic': selected_topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Create a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank of invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
