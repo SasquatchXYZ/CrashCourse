0a037e1bddebd924eb10417dd2361980036d6c83
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-10-11 22:01:23 -0400

    Adding the Topics page

--- Chapter-18/learning_log/learning_logs/views.py
+++ Chapter-18/learning_log/learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    ordered_topics = Topic.objects.order_by('date_added')
+    context = {'topics': ordered_topics}
+    return render(request, 'learning_logs/topics.html', context)
