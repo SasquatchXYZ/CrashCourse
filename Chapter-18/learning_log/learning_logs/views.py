b19e051c12f03a2d1e8ace00ee88e76f7e23b30d
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-10-12 17:46:47 -0400

    Bug Fix in owner/view

--- Chapter-18/learning_log/learning_logs/views.py
+++ Chapter-18/learning_log/learning_logs/views.py
@@ -26,7 +26,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     selected_topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
+    if selected_topic.owner != request.user:
         raise Http404
     entries = selected_topic.entry_set.order_by('-date_added')
     context = {'topic': selected_topic, 'entries': entries}
